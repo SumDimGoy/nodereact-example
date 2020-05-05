51515d726e0d9b8f573fc7490fb33474695c9c42
SumDimGoy <sumdimgoy@gmail.com>
2020-05-04 23:17:50 -0400

    User records in mongoDB associated to cookie session
    Signed-off-by: SumDimGoy <sumdimgoy@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');//allows passport to use cookieSession
+const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
 
-const dbstring = require('./config/keys').mongoURI;
-mongoose.connect(dbstring);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, //cookie lasts for one day (ms)
+    keys: [keys.cookieKey] //array allows multiple keys, chooses one at random
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,33 @@ const keys = require('../config/keys');
 //load models from mongoose
 const User = mongoose.model('users');
 
-//configure google oauth
+passport.serializeUser((user, done) => {
+  /*
+  Serialize the User object.
+
+  done is a context from passport library, call it to transition to next auth
+  phase.
+
+  done(errhdlr, unique_id)
+  errhdlr is the placeholder for the error arg, set null to bypass. unique_id
+  needs to be a unique value to each user.
+
+  Set to the auto-generated value of the '_id' property in the users Mongo
+  collection.
+
+  */
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  //unserialize the user object by user.id
+  User.findById(id)
+  .then(user => {
+    done(null, user);
+  });
+});
+
+//configure google oauth strategy
 passport.use(
   new GoogleStrategy( {
     clientID: keys.googleClientID,
@@ -18,8 +44,22 @@ passport.use(
   //console.log('Refresh Token: ', refreshToken);
   //console.log('profile: ', profile);
   //console.log('Done: ', done);
-  new User({
-    googleID: profile.id,
-    name: profile.displayName
-  }).save();
+
+    User.findOne({ googleID: profile.id })
+      .then(
+        existingUser => {
+          if (existingUser) {
+            //if record already exists
+            done(null, existingUser);
+          }
+          else {
+            new User({
+              googleID: profile.id,
+              name: profile.displayName
+            })
+              .save()
+              .then(user => done(null, user));
+          }
+        }
+     )
 }));
