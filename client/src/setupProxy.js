b14e7b28a4d61ee1938ca669eeedd21f4258705e
SumDimGoy <sumdimgoy@gmail.com>
2020-05-09 01:38:11 -0400

    documentation
    Signed-off-by: SumDimGoy <sumdimgoy@gmail.com>

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
+  /*
+  This proxy configuration assists the development environment in forwarding the
+  original request from the port on which the create-react-app server runs, to
+  the port on which the backend runs. This allows for cookies to be included in
+  the request.
+  */
   app.use(
     [
       "/api",
-      "auth/google" ],
+      "/auth/google" ],
       createProxyMiddleware({
         target: 'http://localhost:5000', })
   );
