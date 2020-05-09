7a9e10859df573a50543f6f40411bc166dae9dd8
SumDimGoy <sumdimgoy@gmail.com>
2020-05-09 01:40:58 -0400

    react app, index, and documentation
    Signed-off-by: SumDimGoy <sumdimgoy@gmail.com>

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hi There
+    </div>
+  );
+};
+
+//allows App to be imported
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -6,66 +6,64 @@ const keys = require('../config/keys');
 //load models from mongoose
 const User = mongoose.model('users');
 
-passport.serializeUser((user, done) => {
-  /*
-  Serialize the User object.
+passport.serializeUser(
+  (user, done) => {
+    /*
+    Serialize the User object.
 
-  passport.serializeUser() returns a function object for associating a user object
-  to a unique string value. done() is a context from passport library, call it
-  to transition to next auth phase.
+    passport.serializeUser() returns a function object for associating a user object
+    to a unique string value. done() is a context from passport library, call it
+    to transition to next auth phase.
 
-  done(errhdlr, id)
-  errhdlr is the placeholder for the on error arg, set null to bypass. id
-  needs to be a unique value to each user. Assigned to the auto-generated value
-  of the '_id' property in the users Mongo collection.
-  */
+    done(errhdlr, id)
+    errhdlr is the placeholder for the on error arg, set null to bypass. id
+    needs to be a unique value to each user. Assigned to the auto-generated value
+    of the '_id' property in the users Mongo collection.
+    */
 
-  //pass the user id (auto generated in mongodb)
-  done(null, user.id);
+    //pass the user id (auto generated in mongodb)
+    done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  //find the user by id stored in db record
-  User.findById(id)
-  .then(user => {
-
-    //pass the user object
+passport.deserializeUser(
+  async(id, done) => {
+    let user = await User.findById(id);
     done(null, user);
-  });
-});
+  }
+);
 
 //configure google oauth strategy
 passport.use(
-  new GoogleStrategy( {
+  new GoogleStrategy ( {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true //trust heroku proxy and preserve https
+    callbackURL: '/auth/google/callback'
   },
-  (accessToken, refreshToken, profile, done) => {
-  //test
-  //console.log('Access Token: ', accessToken);
-  //console.log('Refresh Token: ', refreshToken);
-  //console.log('profile: ', profile);
-  //console.log('Done: ', done);
+  async(accessToken, refreshToken, profile, done) => {
+      //test
+      //console.log('Access Token: ', accessToken);
+      //console.log('Refresh Token: ', refreshToken);
+      //console.log('profile: ', profile);
+      //console.log('Done: ', done);
 
-    //query the user's google ID, filter by profile id
-    User.findOne({ googleID: profile.id })
-      .then(
-        existingUser => {
-          if (existingUser) {
-            //if record already exists, give data to passport
-            done(null, existingUser);
-          }
-          else {
-            //else create new user, commit to db, give data to passport
-            new User({
-              googleID: profile.id,
-              name: profile.displayName
-            })
-              .save()
-              .then(user => done(null, user));
-          }
-        }
-     )
-}));
+      //query the user's google account ID (profile id)
+      let existingUser = await User.findOne({ googleID: profile.id});
+
+      //if the record already exists, return and give data to passport
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      //else create new user
+      let newUser = new User({
+        googleID: profile.id,
+        name: profile.displayName
+      });
+
+      //commit to db
+      await newUser.save();
+
+      //give data to passport
+      done(null, newUser);
+    })
+);
